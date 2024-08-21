db7dfd9596abd8ee761e87cf88a5a741c623737b
jabes <jabesvieira4@gmail.com>
2024-08-21 19:16:19 -0300

    organo

---
+++ projeto/src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import "./Banner.css";
+
+export const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="/img/banner.png" alt="O banner principal da página" />
+    </div>
+  );
+};
