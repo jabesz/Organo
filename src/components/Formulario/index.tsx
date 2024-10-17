6dd9d65b5994ee1313b9077fddefaddc177cec12
jabes <jabesvieira4@gmail.com>
2024-10-17 12:38:29 -0300

    update

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -17,6 +17,7 @@ const Formulario = (props: FormularioProps) => {
   const [cargo, setCargo] = useState("")
   const [imagem, setImagem] = useState("")
   const [time, setTime] = useState("")
+  const [data, setData] = useState("")
   
 
   const aoSalvar = (evento: React.FormEvent<HTMLFormElement>) => {
@@ -58,6 +59,13 @@ const Formulario = (props: FormularioProps) => {
           placeholder="Digite o endereço da imagem" 
           valor={imagem}
           aoAlterado={valor => setImagem(valor)}
+        />
+        <CampoTexto 
+          label = "Data de entrada no time"
+          placeholder = ""
+          valor={data}
+          aoAlterado={valor => setData(valor)}
+          tipo="date"
         />
         <ListaSuspensa
          obrigatorio={true}
